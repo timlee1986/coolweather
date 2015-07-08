de8d04c7f739c5bc5614370a9d245b1fa0a4a24e
timlee1986 <575705420@qq.com>
2015-07-08 12:09:04 +0800

    首次提交

---
+++ src/com/timlee1986/coolweather/ChooseAreaActivity.java
@@ -0,0 +1,17 @@
+package com.timlee1986.coolweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChooseAreaActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_area);
+	}
+}
